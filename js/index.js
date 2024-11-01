8959cbd6b490e67a456750dedcc16381961b0aa4
karonte999 <karonteisalive@gmail.com>
2024-11-01 16:56:51 +0100

    ADDTITO_1_11_CAMBIO2

--- js/index.js
+++ js/index.js
@@ -9,6 +9,55 @@
 
 
 
+// CAROUSEL
+
+
+
+
+
+
+
+
+
+
+const carouselItems = document.querySelectorAll(".carousel_item"); 
+let i = 1;
+
+setInterval(() => {
+// Accessing All the carousel Items
+ Array.from(carouselItems).forEach((item,index) => {
+
+   if(i < carouselItems.length){
+    item.style.transform = `translateX(-${i*100}%)`
+   }
+  })
+
+
+  if(i < carouselItems.length){
+    i++;
+  }
+  else{
+    i=0;
+  }
+},2500)
+
+function costeTotal() {
+  "use strict";
+  var numero = document.getElementById("numero").value;
+  document.getElementById("coste").innerHTML = (numero * 32) + "€";
+
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 //MARQUEE
