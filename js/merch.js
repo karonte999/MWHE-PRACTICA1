09eb369d9959f555add428823ab86bf85318b541
grisel-pacsi <griselepacsi@gmail.com>
2024-11-05 02:20:55 +0100

    ADD_GRISEL_5/11/24 (1)
    Nav ok + modal

--- js/merch.js
+++ js/merch.js
@@ -21,7 +21,32 @@ function reveal() {
     }
 }
 
+// const myModal = document.getElementById('myModal')
+// const myInput = document.getElementById('myInput')
 
+// myModal.addEventListener('shown.bs.modal', () => {
+//   myInput.focus()
+// })
+
+const myModal = new bootstrap.Modal(document.getElementById('exampleModal'), {
+  backdrop: 'static', // Evita que el modal se cierre al hacer clic fuera
+  keyboard: false     // Evita que el modal se cierre con la tecla "Esc"
+});
+
+let modalShown = false; // Variable de estado para rastrear si el modal se ha mostrado
+
+window.addEventListener('scroll', () => {
+  // Mostrar el modal cuando se haya hecho scroll hacia abajo al menos 200 píxeles
+  if (window.scrollY > 200 && !modalShown) {
+      myModal.show(); // Muestra el modal
+      modalShown = true; // Actualiza la variable de estado
+  }
+});
+
+// Escucha el evento de cierre del modal
+document.getElementById('exampleModal').addEventListener('hidden.bs.modal', () => {
+  modalShown = true; // Mantiene la variable en true para no volver a mostrar el modal
+});
 
 
 
